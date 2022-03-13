12443615e05045f9f1fd225cb04596e5dfe8944f
hollan2 <hollan2@pdx.edu>
2022-03-13 00:48:35 -0800

    Formatted previous work experience section.

---
+++ index.js
@@ -0,0 +1,14 @@
+function showAndHide(className) {
+    for (let i = 0; i < 7; i++) {
+        let str = 'descr' + i;
+        let tab = 'tab' + i;
+        if (str == className) {
+            document.getElementsByClassName(str)[0].style.display = 'block';
+            document.getElementsByClassName(tab)[0].style.background = '#555555';
+        } else {
+            document.getElementsByClassName(str)[0].style.display = 'none';
+            document.getElementsByClassName(tab)[0].style.background =
+                'rgb(54, 129, 142)';
+        }
+    }
+}
